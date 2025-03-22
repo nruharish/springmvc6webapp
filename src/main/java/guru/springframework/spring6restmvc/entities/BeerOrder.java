3da414712654f1843b7f17c45a302e97e62ab1fb
nruharish <bs.harish@gmail.com>
2025-03-22 13:11:32 +0530

    beer order entity

---
+++ src/main/java/guru/springframework/spring6restmvc/entities/BeerOrder.java
@@ -0,0 +1,41 @@
+package guru.springframework.spring6restmvc.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.*;
+import org.hibernate.type.SqlTypes;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class BeerOrder {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @UuidGenerator
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+
+    public boolean isNew(){
+        return this.id ==null;
+    }
+
+    private String customerRef;
+
+}
